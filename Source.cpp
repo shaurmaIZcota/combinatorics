731ff8fa581cf9b8aacad80032560b65aef15e0b
Roman Kubrak <44610761+shaurmaIZcota@users.noreply.github.com>
2021-12-23 19:11:46 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
-int factor(int N)
+long long factor(int N)
 {
     int F = 1;
     for (int i = 1; i <= N; i++){
@@ -9,30 +10,52 @@ int factor(int N)
     }
     return F;
 }
-int accommodation(int a, int b) {
+long long accommodation(int a, int b) {
     return factor(a) / factor(a - b);
 }
-int combinations (int a, int b) {
+long long combinations (int a, int b) {
     return factor(a) / (factor(b) * factor(a - b));
 }
-int permutations (int a, int b) {
+long long permutations (int a, int b) {
     return factor(a);
 }
+
+long long rep_accommodation(int a, int b) {
+    return pow(a,b);
+}
+long long rep_combinations(int a, int b) {
+    return factor(a+b-1) / (factor(b) * factor(a - 1 ));
+}
+long long rep_permutations(int a, int b) {
+    return factor(a);
+}
+
 int main() {
     setlocale(LC_ALL, "Russian");
     int a = 0, b = 0;
     int in;
+    bool repetitions=0;
     cout << "Программа формул комбинаторики"<<endl;
     cout << "Введите соответствующие число" << endl;
     cout << "1) Размещения\n2) Сочетания\n3) Перестановки\n";
     cin >> in;
-    cout << "Введите числа: ";
+    cout << "Есть ли повторения? Введите если да- введите 1, если нет - введите 0: ";
+    cin >> repetitions;
     do {
+        cout << "Введите 2 числа, первое из них должно быть больше второго: ";
         cin >> a >> b;
-    } while (a <= 0 && b <= 0);
-    if (in == 1) { cout << "Ответ: " << accommodation(a, b); }
-    if (in == 2) { cout << "Ответ: " << combinations(a, b); }
-    if (in == 3) { cout << "Ответ: " << permutations(a, b); }
+    } while (a <= 0 || b <= 0 || b>a);
+    if (repetitions) {
+        if (in == 1) { cout << "Ответ: " << rep_accommodation(a, b); }
+        if (in == 2) { cout << "Ответ: " << rep_combinations(a, b); }
+        if (in == 3) { cout << "Ответ: " << rep_permutations(a, b); }
+    }
+    else{
+        if (in == 1) { cout << "Ответ: " << accommodation(a, b); }
+        if (in == 2) { cout << "Ответ: " << combinations(a, b); }
+        if (in == 3) { cout << "Ответ: " << permutations(a, b); }
+    }
+
     
 
     return 0;
